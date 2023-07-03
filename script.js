f907ef041c62fe764c9ba27a1f498038aef14f03
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-03 22:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function delayedHello() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Hello, world!");
+        }, 1000);
+      });
+    }
+
+    delayedHello().then((message) => {
+      document.getElementById("output").textContent = message;
+    });
